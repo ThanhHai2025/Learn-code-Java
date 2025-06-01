4c1a735b3b0c2592fb5ffea79e3cb43adc3d7aac
ThanhHai2025 <haingothak@gmail.com>
2025-06-01 21:10:02 +0700

    Hello world

---
+++ Slot1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slot1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Slot1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        // TODO code application logic here
+    }
+    
+}
