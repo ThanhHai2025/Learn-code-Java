942dbdeb89661edf504e55a32450dd2a3c8cd04c
ThanhHai2025 <haingothak@gmail.com>
2025-06-10 15:56:44 +0700

    StringDemo2

---
+++ StringDemo2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringdemo;
+
+import java.util.Scanner;
+public class StringDemo2 {
+    public static void main(String aegs[]){
+        Scanner sc=new Scanner(System.in);
+        String origin, replaced, replacement;
+        System.out.print("Enter origninal string: ");
+        origin=sc.nextLine();
+        System.out.print("Enter replaced string: ");
+        replaced=sc.nextLine();
+        System.out.print("Enter replacing string");
+        replacement=sc.nextLine();
+        origin = origin.replaceAll(replaced, replacement);
+        System.out.println("After replacing:" + origin);
+        System.out.println("Upperace:" + origin.toUpperCase());
+        System.out.println("Origin:" + origin);
+        System.out.print("Enter the index of extracted character:");
+        int index=Integer.parseUnsignedInt(sc.nextLine());
+        System.out.format("The %d (th)character: %c\n", index, origin.charAt(index));
+    }
+}
+
